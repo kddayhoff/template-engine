581db38031e13673de87e42f5cdab87b4caa6e73
Karla Dayhoff <kddayhoff@gmail.com>
2020-04-15 00:07:55 -0500

    got employee to pass test, nearly got engineer to pass test, the intern is failing the test

--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -1,2 +1,26 @@
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
 
+const Employee = require("./Employee");
+
+class Engineer extends Employee {
+    constructor(name, id, email, github, GitHubUser) {
+      super(name, id, email);
+      this.github = github;
+      this.GitHubUser = this.GitHubUser;
+    }
+
+    github() {
+        return this.github;
+        return this.GitHubUser;
+    }
+
+    getRole() {
+        return "Engineer";
+    }
+
+}
+
+const engineer = new Engineer(name, 1, email, github, GitHubUser, "Engineer");
+
+console.log(engineer)
+
