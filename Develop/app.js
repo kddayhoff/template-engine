93eb1c98db64beceeeb88567d2738f284e799f03
Karla Dayhoff <kddayhoff@gmail.com>
2020-04-16 22:16:52 -0500

    i don'tknow

--- Develop/app.js
+++ Develop/app.js
@@ -1,15 +1,29 @@
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
-
+const path = require("path");
 const fs = require("fs");
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 const render = require("./lib/htmlRenderer");
 const inquirer = require("inquirer");
-const path = require("path");
 
 
+let team = []; 
+//ask base Employee questions
+    
+         
+const getTeam = () => {
+    inquirer
+    .prompt ([
+       {
+          type: "list",
+          message: "What would you like to do?",
+          choices: ["Build Team", "Finish Team"],
+          name: "newTeam",
+        }
+    ])
+}
 
 function getInfo() {
     inquirer
@@ -42,28 +56,17 @@ function getInfo() {
             name: "role",
           }]
         )
-        }
-          const team = []; 
-         
-          const getTeam = () => {
-              inquirer
-              .prompt ([
-                 {
-                    type: "list",
-                    message: "What would you like to do?",
-                    choices: ["Build Team", "Finish Team"],
-                    name: "newTeam",
-                  }
-              ])
-          
+    // build team with const team empty array    
+  
           .then((response) =>  {
+              console.log(response)
               const buildTeam = response.newTeam;
               switch (buildTeam) {
                   case "Build Team":
                       inquirer
                       .prompt(questions)
                       .then((choices) => {
-                
+          //should the user choose Intern:      
                         if (choices === "Intern") {
                         console.log("intern");
                         inquirer
@@ -75,25 +78,58 @@ function getInfo() {
                         .then((answerInt) => {
                             let newIntern = new Intern(
                                 choices.name, choices.id, choices.email, answerInt.school);
-                            team.push(newIntern);   
+                            team.push(newIntern);    
+                        getTeam()  
                      });
-                    
                     }
-                })
-              break;
-        case "Finish Team":
-          console.log(team);
-          if (team.length > 0) {
-            console.log("all done!");
+            //should the user choose Engineer:
+                        else if (choices === "Engineer") {
+                            console.log("engineer");
+                            inquirer
+                            .prompt( {
+                                type: "input",
+                                message: "What is your Github username?",
+                                name: "github"
+                            })
+                        .then((answerEng) => {
+                          let newEngineer = new Engineer
+                             (choices.name, choices.id, choices.email, answerEng.github);
+                          team.push(newEngineer);
+                         getTeam();      
+                        });
+                    }
+             //Should the user choose Manager:       
+                        else (choices === "Manager") 
+                            console.log("manager");
+                            inquirer
+                            .prompt( {
+                                type: "input",
+                                message: "What is your office phone number",
+                                name: "number"
+                            })
+                        .then((answerMan) => {
+                          let newManager = new Manager(choices.name, choices.id, choices.email, answerMan.number);
+                          team.push(newManager);
+                        getTeam();    
+                        })
+                    });
+                break;
+         //should the user wish to finish team:       
+            case "Finish Team":
+                console.log(team);
+              if (team.length > 0) {
+                console.log("all done!");
             writeHTML(render(team));
-          } else {
+            } 
+            else {
             console.log("no team members");
-          }
+           }
           break;
         default:
           break;
             }
-        })
-    }
+        })   
+    }  
+
     getInfo();
     
